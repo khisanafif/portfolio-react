2abf524694647d435707558fd95b1c41e5d57104
khisanafif <khisanafif32@gmail.com>
2024-06-07 04:35:18 +0700

    Update foto dan Contact

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -34,7 +34,7 @@ export default function ContactMe() {
         <p className="sub--title">Get In Touch</p>
         <h2>Contact Me</h2>
         <p className="text-lg">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
+          Contact Me Here !!!
         </p>
       </div>
       <form className="contact--form--container" onSubmit={handleSubmit}>
